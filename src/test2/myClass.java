25b6756b11467b9efd130630b56508320759f097
lcg0209 <cg.lee@dandinet.com>
2014-09-23 12:55:17 +0900

    second

---
+++ src/test2/myClass.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class myClass {
+	String name;
+	public myClass(String name) {
+		this.name = name;
+	}
+	public void print(){
+		System.out.println(this.name);
+	}
+}
--- src/test2/test.java
+++ src/test2/test.java
@@ -3,5 +3,7 @@ package test2;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		myClass obj = new myClass("test");
+		obj.print();
 	}
 }
